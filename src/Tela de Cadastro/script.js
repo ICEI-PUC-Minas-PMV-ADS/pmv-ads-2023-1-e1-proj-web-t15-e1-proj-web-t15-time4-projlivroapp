80dc84e79bb6c73fa61fb33363d440cf788ea646
Ana Clara <anarozinholi@gmail.comgit config --global user.name Ana>
2023-05-30 19:37:08 -0300

    alteração js

--- src/Tela de Cadastro/script.js
+++ src/Tela de Cadastro/script.js
@@ -1,19 +1,44 @@
-const form = document.getElementById("form");
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const passwordConfirmation = document.getElementById("password-confirmation");
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('form');
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-  checkInputs();
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirmation = document.getElementById('password-confirmation').value;
+    const date = document.getElementById('date').value;
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+
+    const user = {
+      username,
+      email,
+      password,
+      date,
+      gender
+    };
+
+
+    if (password !== passwordConfirmation) {
+      alert('As senhas devem ser iguais.');
+      return;
+    }
+
+    localStorage.setItem('user', JSON.stringify(user));
+
+    alert('Cadastro realizado com sucesso!');
+    window.location.href="login/home.html"
+
+
+  });
 });
+  /*checkInputs();*/
 
-function checkInputs() {
+
+  /*function checkInputs() {
     const usernameValue = username.value;
-    const emailValue = email.value;
-    const passwordValue = password.value;
+    const password = password.value;
     const passwordConfirmationValue = passwordConfirmation.value;
   
     if (usernameValue === "") {
@@ -22,17 +47,17 @@ function checkInputs() {
       setSuccessFor(username);
     }
   
-    if (emailValue === "") {
+    if (email === "") {
       setErrorFor(email, "O email é obrigatório.");
-    } else if (!checkEmail(emailValue)) {
+    } else if (!checkEmail(email)) {
       setErrorFor(email, "Por favor, insira um email válido.");
     } else {
       setSuccessFor(email);
     }
   
-    if (passwordValue === "") {
+    if (password === "") {
       setErrorFor(password, "A senha é obrigatória.");
-    } else if (passwordValue.length < 7) {
+    } else if (password.length < 7) {
       setErrorFor(password, "A senha precisa ter no mínimo 7 caracteres.");
     } else {
       setSuccessFor(password);
@@ -61,22 +86,23 @@ function checkInputs() {
     const formControl = input.parentElement;
     const small = formControl.querySelector("small");
   
-    
+    // Adiciona a mensagem de erro
     small.innerText = message;
   
-    
+    // Adiciona a classe de erro
     formControl.className = "form-control error";
   }
   
   function setSuccessFor(input) {
     const formControl = input.parentElement;
   
-   
+    // Adicionar a classe de sucesso
     formControl.className = "form-control success";
   }
   
-  function checkEmail(email) {
+    function checkEmail(email) {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
       email
     );
-  }
+  } */
+
